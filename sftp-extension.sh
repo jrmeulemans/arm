180b5d96b1d93f97d389386c2b28e681da12be69
jrmeulemans <57834841+jrmeulemans@users.noreply.github.com>
2020-07-31 14:41:18 -0400

    Update sftp-extension.sh

--- sftp-extension.sh
+++ sftp-extension.sh
@@ -25,7 +25,7 @@ sudo mkdir /sftp
 
 # Create blobfuse.service config file
 
-VAR1="rjiB+NNtKDmbnmimEpJ7F4igKVaHu1iGjwlqPWRc7TnsnNEStJHUWmtPIDe9hvgpDtVFbequs64txg8fTNhDvg=="
+VAR1=""
 sudo wget https://raw.githubusercontent.com/jrmeulemans/arm/master/blobfuse.service
 sudo sed -i 's+PARAM1+/sftp+g' blobfuse.service
 sudo sed -i 's+PARAM2+sftp+g' blobfuse.service
